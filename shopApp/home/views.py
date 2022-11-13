5e074710d49ade245e8131d62ec6994aa728584f
Tomasz Bułecki <tomekbu.ecki@gmail.com>
2022-11-13 16:53:56 +0100

    changed config for live server inside docker container

--- shopApp/home/views.py
+++ shopApp/home/views.py
@@ -6,4 +6,8 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>test</h1>")
+    return HttpResponse("<h1>test1</h1>")
+
+
+def second_test(request):
+    return HttpResponse("<h1>test1</h1>")
